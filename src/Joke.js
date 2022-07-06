cbeef15588068b52e6510242d13d9fdc67adf9fb
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-06 18:16:44 +0700

    work on Joke component
    -install and use font awesome
    -minor basic styling

--- src/Joke.js
+++ src/Joke.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import './Joke.css';
 
 class Joke extends Component {
   render() {
     return (
       <div className="Joke">
-        <div>
-          <button>UP</button>
-          <div>{this.props.votes}</div>
-          <button>DOWN</button>
+        <div className="Joke-buttons">
+          <FontAwesomeIcon
+            icon={faArrowUp}
+            onClick={this.handleVote}
+          ></FontAwesomeIcon>
+          <span>{this.props.votes}</span>
+          <FontAwesomeIcon
+            icon={faArrowDown}
+            onClick={this.handleVote}
+          ></FontAwesomeIcon>
         </div>
-        <p>{this.props.joke}</p>
+        <div className="Joke-text">{this.props.joke}</div>
         <img alt="smiley" />
       </div>
     );
