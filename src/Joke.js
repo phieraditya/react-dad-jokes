b59a85c9874b691f0bb2399138668f61158028ce
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-07 15:52:21 +0700

    add dynamic emojis based on votes

--- src/Joke.js
+++ src/Joke.js
@@ -21,6 +21,23 @@ class Joke extends Component {
       return '#f44336';
     }
   }
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return 'em em-rolling_on_the_floor_laughing';
+    } else if (this.props.votes >= 12) {
+      return 'em em-laughing';
+    } else if (this.props.votes >= 9) {
+      return 'em em-smiley';
+    } else if (this.props.votes >= 6) {
+      return 'em em-slightly_smiling_face';
+    } else if (this.props.votes >= 3) {
+      return 'em em-neutral_face';
+    } else if (this.props.votes >= 0) {
+      return 'em em-confused';
+    } else {
+      return 'em em-angry';
+    }
+  }
   render() {
     return (
       <div className="Joke">
@@ -41,7 +58,7 @@ class Joke extends Component {
         </div>
         <div className="Joke-text">{this.props.joke}</div>
         <div className="Joke-smiley">
-          <i className="em em-rolling_on_the_floor_laughing"></i>
+          <i className={this.getEmoji()}></i>
         </div>
       </div>
     );
