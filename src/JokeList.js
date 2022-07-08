4ad1d2f1deef87c420ec778b1f48634300737656
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-08 15:23:51 +0700

    add a loading spinner

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLaugh } from '@fortawesome/free-solid-svg-icons';
 import Joke from './Joke';
 import './JokeList.css';
 
@@ -14,23 +16,25 @@ class JokeList extends Component {
     super(props);
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+      isLoading: false,
     };
     this.getNewJoke = this.getNewJoke.bind(this);
     this.handleVote = this.handleVote.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
     if (this.state.jokes.length === 0) this.getNewJoke();
   }
   async getNewJoke() {
-    let jokes = [];
-    while (jokes.length < this.props.numJokesToGet) {
+    let newJokes = [];
+    while (newJokes.length < this.props.numJokesToGet) {
       let response = await axios.get(API_URL, {
         headers: { Accept: 'application/json' },
       });
-      jokes.push({ id: uuidv4(), joke: response.data.joke, votes: 0 });
+      newJokes.push({ id: uuidv4(), joke: response.data.joke, votes: 0 });
     }
     this.setState(
-      (st) => ({ jokes: [...jokes, ...st.jokes] }),
+      (st) => ({ jokes: [...newJokes, ...st.jokes], isLoading: false }),
       () =>
         window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
     );
@@ -46,7 +50,23 @@ class JokeList extends Component {
         window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
     );
   }
+  handleClick() {
+    this.setState({ isLoading: true }, this.getNewJoke);
+  }
   render() {
+    if (this.state.isLoading) {
+      return (
+        <div className="JokeList-spinner">
+          <FontAwesomeIcon
+            icon={faLaugh}
+            size="10x"
+            spin
+            inverse
+          ></FontAwesomeIcon>
+          <h1 className="JokeList-title">Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
@@ -58,7 +78,7 @@ class JokeList extends Component {
             alt="laugh icon"
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
           />
-          <button className="JokeList-getmore" onClick={this.getNewJoke}>
+          <button className="JokeList-getmore" onClick={this.handleClick}>
             New Jokes
           </button>
         </div>
